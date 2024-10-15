656932352fdabce9b0cdc4501ce4c4015561fc82
kyallss <150505408+kyallss@users.noreply.github.com>
2024-10-15 23:42:10 +0600

    Update test.java

--- test.java
+++ test.java
@@ -1,35 +1,33 @@
 package org.example;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class TestStringProcessor {
-    public static void main(String[] args) {
-        StringProcessor processor = new StringProcessor();
+class StringProcessorTest {
+    StringProcessor processor = new StringProcessor();
 
-        System.out.println("isStrongPassword Tests:");
-        System.out.println(processor.isStrongPassword("kyiallll"));
-        System.out.println(processor.isStrongPassword("passwordpassword"));
-        System.out.println(processor.isStrongPassword("12345678"));
-        System.out.println(processor.isStrongPassword("adminAdmin"));
-        System.out.println(processor.isStrongPassword("PASSWORD1"));
+    @Test
+    void isStrongPassword() {
+            assertTrue(processor.isStrongPassword("StrongP@ss1"));
+            assertFalse(processor.isStrongPassword("weakp@ss1"));
+            assertFalse(processor.isStrongPassword("StrongPass@"));
+            assertFalse(processor.isStrongPassword("StrongPass1"));
+    }
 
-        System.out.println("\ncalculateDigits Tests:");
-        System.out.println(processor.calculateDigits("There are 123 numbers."));
-        System.out.println(processor.calculateDigits("No digits here"));
-        System.out.println(processor.calculateDigits("1a2b3c4d5e"));
-        System.out.println(processor.calculateDigits("Today is monday"));
-        System.out.println(processor.calculateDigits("The year is 2024"));
+    @Test
+    void calculateDigits() {
+        assertEquals(4, processor.calculateDigits("There are 1234 numbers"));
+        assertEquals(0, processor.calculateDigits("No digits here"));
+    }
 
-        System.out.println("\ncalculateWords Tests:");
-        System.out.println(processor.calculateWords("Hello world!"));
-        System.out.println(processor.calculateWords("    "));
-        System.out.println(processor.calculateWords("Hello my name is Kyial"));
-        System.out.println(processor.calculateWords("   Multiple   spaces    in   between   words."));
-        System.out.println(processor.calculateWords(""));
+    @org.junit.jupiter.api.Test
+    void calculateWords() {
+        assertEquals(5, processor.calculateWords("This is a simple sentence"));
+        assertEquals(0, processor.calculateWords(""));
+    }
 
-        System.out.println("\ncalculateExpression Tests:");
-        System.out.println(processor.calculateExpression("2 + 3 * 4"));
-        System.out.println(processor.calculateExpression("(1 + 2) * 3"));
-        System.out.println(processor.calculateExpression("10 / 2 + 5"));
-        System.out.println(processor.calculateExpression("100 - (20 + 30)"));
-        System.out.println(processor.calculateExpression("5 * (3 + 2) - 10 / 2"));
+    @org.junit.jupiter.api.Test
+    void calculateExpression() {
+        assertEquals(5.0, processor.calculateExpression("2 + 3"), 0.001);
+        assertEquals(14.0, processor.calculateExpression("2 * (3 + 4)"), 0.001);
     }
 }
